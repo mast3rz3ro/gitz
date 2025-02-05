d3b03e1df173ded4949d576d467d90350b2664aa
mast3rz3ro <detective33322@gmail.com>
2025-02-05 03:22:39 +0300

    some improvments

--- gitz.sh
+++ gitz.sh
@@ -1,78 +1,73 @@
 #!/bin/env bash
 
 if [ ! -x "$(which git)" ]; then
-   printf "git is missing.\n"
-   exit 1
+	echo "git is missing."
+	exit 1
 fi
 
 
 
 
-usage ()
+usage()
 {
-printf "usage: gitz [parameter]
+	echo -e "\
+usage: gitz [parameter]
  setup or s (setup git for first run)
  clone or c (clone repo with their username)
  clean or c2 (clean repo from garbage files)
- reset or r (permanently reset into initial commit)
-"
+ reset or r (permanently reset into initial commit)"
 exit 0
 }
 
 
 if [ -s "./gitz.sh" ] && [ -z "$1" ]; then
-   printf "Installing: '$PREFIX/bin/gitz'\n"
-   cp "./gitz.sh" "$PREFIX/bin/gitz"
+	echo "Installing: '$PREFIX/bin/gitz'"
+	cp "./gitz.sh" "$PREFIX/bin/gitz"
 elif [ "$1" = "clone" ] || [ "$1" = "c" ]; then
-   u="$(printf $2 | sed 's/.*github.com\///; s/\/.*//')"
-   r="$(printf $2 | sed 's/.*github.com\/.*\///')"
-   git clone "$2" "${r}_${u}"
+	u="$(basename "$2")"
+	r="$(dirname "$2")"
+	git clone "$2" "${r}_${u}"
 elif [ "$1" = "setup" ] || [ "$1" = "s" ]; then
-   read -p "username: " u
-   read -p "email: " e
-   read -p "token: " t
-  if [ ! -z "$u" ] && [ ! -z "$e" ] && [ ! -z "$t" ]; then
-   read -p "Override your current settings (y/n): " choice
-  if [ "$choice" = "y" ]; then
-   git config --global init.defaultBranch main
-   git config --global credential.helper store
-   git config --global user.name "$u"
-   git config --global user.email "$e"
-   printf "Saving token into: "~.git-credentials
-   printf "https://${u}:${t}@github.com">~/.git-credentials
-  else
-   printf "Aborting..\n"
-  fi
- fi
+		read -p "username: " u
+		read -p "email: " e
+		read -p "token: " t
+	if [ ! -z "$u" ] && [ ! -z "$e" ] && [ ! -z "$t" ]; then
+			read -p "Override your current settings (y/n): " choice
+		if [ "$choice" = "y" ]; then
+			git config --global init.defaultBranch main
+			git config --global credential.helper store
+			git config --global user.name "$u"
+			git config --global user.email "$e"
+			echo "Saving token into: "~.git-credentials
+			echo "https://${u}:${t}@github.com">~/.git-credentials
+		else
+			echo "Aborting..."
+		fi
+	fi
 elif [ "$1" = "reset" ] || [ "$1" = "r" ]; then
-   printf "Reseting the repo ..\n"
-   printf "WARNING: THIS CAN NOT BE UNDONE !\n"
-   read -p "Continue ? (y/n)" choice
- if [ "$choice" = "y" ]; then
-   b="$(git rev-parse --abbrev-ref HEAD)"
-  if [ -z "$b" ]; then
-   b="main"
-  fi
-   git config --global init.defaultBranch "$b"
-   r="$(git config --get remote.origin.url)"
-   printf "Repo origin: '$r'\n"
-   printf "Branch: '$b'\n"
-   rm -rf .git
-   git init
-   git add .
-   git commit -m "Initial commit"
-   git remote add origin "$r"
-   git status
-   read -p "Are you sure want to push the changes (y/n): " choice
-   if [ "$choice" = "y" ]; then
-     sleep 5
-     git push -u --force origin "$b"
-   else
-     printf "Aborting ..\n"
-   fi
- else
-   printf "Aborting ..\n"
- fi
+		echo "Reseting the repo..."
+		echo "WARNING: THIS CAN NOT BE UNDONE !"
+		read -p "Continue ? (y/n)" choice
+	if [ "$choice" = "y" ]; then
+			b="$(git rev-parse --abbrev-ref HEAD)"
+			[ -z "$b" ] && b="main"
+			git config --global init.defaultBranch "$b"
+			r="$(git config --get remote.origin.url)"
+			echo "Repo origin: '$r'"
+			echo "Branch: '$b'"
+			rm -rf ./.git
+			git init; git add .; git commit -m "Initial commit"
+			git remote add origin "$r"; git status
+			read -p "Are you sure want to push the changes (y/n): " choice
+		if [ "$choice" = "y" ]; then
+			sleep 5
+			git push -u --force origin "$b"
+		else
+			echo "Aborting..."
+		fi
+	else
+		echo "Aborting..."
+	fi
 elif [ "$1" = "clean" ] || [ "$1" = "c2" ]; then
    g="\
    autom4te.cache
@@ -98,24 +93,18 @@ elif [ "$1" = "clean" ] || [ "$1" = "c2" ]; then
    stamp-h1
    Makefile
 "
-   make clean >/dev/null 2>&1
-  for x in $g; do
-    x="$(find . -name "$x" 2>/dev/null | tr '\n' ' ')"
-   for r in $x; do
-      rm -rf "$r" >/dev/null 2>&1
-     if [ ! -z "$r" ] && [ "$0" != "0" ]; then
-      printf "error: rm -rf '$r'\n"
-      #printf "error [$0]: rm -rf '$r'\n"
-     fi
-   done
+		make clean >/dev/null 2>&1
+	for x in $g; do
+		echo "Removing: $x"
+		find ./ -name "$x" -exec rm -rf {} +
   done
-   git status
-  if [ "$(git status | grep -coF "modified:")" != "0" ] || [ "$(git status | grep -coF "deleted:")" != "0" ]; then
-   c="$(git log --oneline -n1 | awk '{print $1}')"
-   printf "An error occurred, reseting into last commit: $c\n"
-   git reset --hard "$c"
-  fi
+		git status
+	if [ "$(git status | grep -coF "modified:")" != "0" ] || [ "$(git status | grep -coF "deleted:")" != "0" ]; then
+		c="$(git log --oneline -n1 | awk '{print $1}')"
+		echo "An error occurred, reseting into last commit: $c"
+		git reset --hard "$c"
+	fi
 else
-   usage
+		usage
 fi
 
